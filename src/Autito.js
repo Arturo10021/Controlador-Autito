c567a19ff5dc0d6c5d4d9db0fa1f3ec30b66162e
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-09-01 19:11:59 -0400

    correccion errores

--- src/Autito.js
+++ src/Autito.js
@@ -13,16 +13,26 @@ export class Auto{
   }
 
   setGrid(comandos) {
-    const gridData = comandos.split('/')[0].split(',');
-    this.maxX = parseInt(gridData[0], 10); 
-    this.maxY = parseInt(gridData[1], 10);
+    const gridData = this.getComandoDimension(comandos);
+    const parte1 = gridData.split(',');
+      this.maxX = parseInt(parte1[0]);
+      this.maxY = parseInt(parte1[1]);
   }
 
   setPosicion(comandos) {
-    const positionData = comandos.split('/')[1];
-    const posData = positionData.split(',');
-    this.X = parseInt(posData[0], 10);
-    this.Y = parseInt(posData[1], 10);
+    const positionData = this.getComandoPosicion(comandos);
+    const parte2 = positionData.split(',');
+      this.X = parseInt(parte2[0]);
+      this.Y = parseInt(parte2[1]);
   }
+
+  getComandoDimension(comandos) {
+    return comandos.split('/')[0];
+  }
+
+  getComandoPosicion(comandos) {
+    return comandos.split('/')[1];
+  }
+
 }
   export default Auto;
--- src/Autito.test.js
+++ src/Autito.test.js
@@ -3,25 +3,25 @@ import Auto from "./Autito";
 describe("Auto", () => {
   it("Mostrar dimension X", () => {
     const auto = new Auto();
-    auto.Inicializar("5,5");
+    auto.Inicializar("5,5/1,2N/IAIAIAIAA");
     expect(auto.maxX).toBe(5);
   });
 
   it("Mostrar dimension Y", () => {
     const auto = new Auto();
-    auto.Inicializar("5,5");
+    auto.Inicializar("5,5/1,2N/IAIAIAIAA");
     expect(auto.maxY).toBe(5);
   });
 
   it("Mostrar Posición inicial X", () => {
     const auto = new Auto();
-    auto.Inicializar("5,5/1,2");
+    auto.Inicializar("5,5/1,2N/IAIAIAIAA");
     expect(auto.X).toBe(1);
   });
 
   it("Mostrar Posición inicial Y", () => {
     const auto = new Auto();
-    auto.Inicializar("5,5/1,2");
+    auto.Inicializar("5,5/1,2N/IAIAIAIAA");
     expect(auto.Y).toBe(2);
   });
 });
